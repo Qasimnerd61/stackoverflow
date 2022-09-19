bc8e143293f3a9cef3af0d78ac3469978cefa3cd
Qasimnerd61 <108872345+Qasimnerd61@users.noreply.github.com>
2022-09-19 11:04:49 -0400

    Add files via upload

---
+++ New folder (4)/StackOverFlow/DAL/Questions_DAL.cs
@@ -0,0 +1,39 @@
+﻿using StackOverFlow.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace StackOverFlow.DAL
+{
+    public class Questions_DAL
+    {
+        SqlConnection sqlCon = new SqlConnection(ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString);
+        public List<Questions> getAllQuestions()
+        {
+            sqlCon.Open();
+            SqlCommand sqlCmd_getAllMovies = new SqlCommand("getAllQuestions", sqlCon);
+            sqlCmd_getAllMovies.CommandType = CommandType.StoredProcedure;
+
+            SqlDataAdapter adapter = new SqlDataAdapter(sqlCmd_getAllMovies);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            List<Questions> allQuestionsList = new List<Questions>();
+            Dictionary<string, object> childRow;
+            foreach (DataRow row in table.Rows)
+            {
+                childRow = new Dictionary<string, object>();
+                foreach (DataColumn col in table.Columns)
+                {
+                    childRow.Add(col.ColumnName, row[col]);
+                }
+                allQuestionsList.Add(new Questions(childRow));
+            }
+            sqlCon.Close();
+            return allQuestionsList;
+        }
+}
---
+++ New folder (4)/StackOverFlow/Models/Questions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StackOverFlow.Models
+{
+    public class Questions
+    {
+        public int questionID;
+        public string questionTitle;
+        public string questionBody;
+        public int quesVoteCount;
+        public DateTime postDate;
+        public List<Tags> tags=new List<Tags>();
+        public List<Answers> answers = new List<Answers>();
+
+
+        public Questions(int _questionId, string _questionTitle, string _questionBody, int _quesVoteCount, DateTime _postDate, List<Tags> _tags, List<Answers> _answers)
+        {
+            questionID = _questionId;
+            questionTitle = _questionTitle;
+            questionBody =_questionBody;
+            quesVoteCount = _quesVoteCount;
+            postDate = _postDate;
+            tags = _tags;
+            answers = _answers;
+        }
+
+        public Questions(Dictionary<string, object> childRow)
+        {
+            questionID = int.Parse(childRow["MovieID"].ToString());
+            questionTitle = childRow["Name"].ToString();
+            length = childRow["Length"].ToString();
+            price = int.Parse(childRow["Price"].ToString());
+            poster = childRow["Poster"].ToString();
+
+            List<int> allGenreIDs = new List<int>();
+
+            allGenreIDs = movies_DAL_Obj.getAllGenreIDs(int.Parse(childRow["MovieID"].ToString()));
+
+            //int count = 0;
+            foreach (var item in allGenreIDs)
+            {
+                Genres tempGenre = new Genres();
+                tempGenre.ID = item;
+                tempGenre.name = movies_DAL_Obj.getGenreName(item);
+
+                genreList.Add(tempGenre);
+                //count++;                
+            }
+        }
+    }
+}
